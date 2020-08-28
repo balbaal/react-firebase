3e5e75a82eae807111a510d1376f701adce775ee
Balbaal <iqbal.mi20@gmail.com>
2020-08-28 11:15:04 +0800

    add: register user

--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,8 +1,60 @@
 import React from "react";
+import firebase from "configs/firebase";
 
 class Register extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  _handleOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  _handleSubmit = () => {
+    const { email, password } = this.state;
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(function (res) {
+        console.log(res, ">> response success");
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(error, ">>> error");
+        // ...
+      });
+  };
+
   render() {
-    return <div>register page</div>;
+    const { email, password } = this.state;
+
+    return (
+      <div>
+        <input
+          value={email}
+          onChange={this._handleOnChange}
+          value={email}
+          placeholder="email..."
+          type="text"
+          name="email"
+        />
+        <input
+          value={password}
+          onChange={this._handleOnChange}
+          placeholder="password..."
+          type="password"
+          name="password"
+        />
+        <button onClick={this._handleSubmit}>register</button>
+      </div>
+    );
   }
 }
 
