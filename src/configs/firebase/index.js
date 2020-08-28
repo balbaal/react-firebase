97ed053520a3898a86543977cbf8729f8cfbd2b1
Balbaal <iqbal.mi20@gmail.com>
2020-08-28 20:53:12 +0800

    changes: consume variable env for firebase

--- src/configs/firebase/index.js
+++ src/configs/firebase/index.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyD4ddy4_DlUK82nELhLnQHTEwEmce0pHc4",
-  authDomain: "react-firebase-todo-4d3f6.firebaseapp.com",
-  databaseURL: "https://react-firebase-todo-4d3f6.firebaseio.com",
-  projectId: "react-firebase-todo-4d3f6",
-  storageBucket: "react-firebase-todo-4d3f6.appspot.com",
-  messagingSenderId: "334743776764",
-  appId: "1:334743776764:web:fa9cbdcedfef767a5d1679",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
