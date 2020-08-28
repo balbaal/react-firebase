40b8899ae39ec6d90152fb55545ddebdb4e14844
Balbaal <iqbal.mi20@gmail.com>
2020-08-28 22:56:16 +0800

    add: login system connected to fb by user pass

--- src/configs/redux/actions/index.js
+++ src/configs/redux/actions/index.js
@@ -36,3 +36,32 @@ export const actionRegisterUser = (payload) => (dispatch) => {
       });
     });
 };
+
+export const actionLoginUser = (payload) => (dispatch) => {
+  dispatch({
+    type: SET_LOADING,
+    payload: true,
+  });
+
+  return firebase
+    .auth()
+    .signInWithEmailAndPassword(payload.email, payload.password)
+    .then(function (res) {
+      console.log(res, ">>> response");
+      dispatch({
+        type: SET_LOADING,
+        payload: false,
+      });
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(error, ">>> error");
+      dispatch({
+        type: SET_LOADING,
+        payload: false,
+      });
+    });
+};
--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -1,9 +1,65 @@
 import React from "react";
+import { connect } from "react-redux";
+
+// Components
+import Button from "components/atoms/Button";
+
+// Actions
+import { actionLoginUser } from "configs/redux/actions";
 
 class Login extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  _handleOnChange = (e) => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  _handleSubmit = () => {
+    const { email, password } = this.state;
+    const { actionLoginUser } = this.props;
+
+    actionLoginUser({ email, password });
+  };
+
   render() {
-    return <div>login page</div>;
+    const { email, password } = this.state;
+    const { isLoading } = this.props;
+
+    return (
+      <div>
+        <input
+          value={email}
+          onChange={this._handleOnChange}
+          value={email}
+          placeholder="email..."
+          type="text"
+          name="email"
+        />
+        <input
+          value={password}
+          onChange={this._handleOnChange}
+          placeholder="password..."
+          type="password"
+          name="password"
+        />
+        <Button isLoading={isLoading} onClick={this._handleSubmit}>
+          login
+        </Button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+  return {
+    isLoading: state.isLoading,
+  };
+};
+
+export default connect(mapStateToProps, { actionLoginUser })(Login);
