c1cec0d1acb3b48abaa74e1a1e394e9c4bd4604f
Balbaal <iqbal.mi20@gmail.com>
2020-08-28 22:33:37 +0800

    refactor: moved routing page to configs

---
+++ src/configs/routes/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Components
+import Login from "containers/pages/Login";
+import Register from "containers/pages/Register";
+
+const Home = () => {
+  return <div>home page</div>;
+};
+
+const NotFound = () => {
+  return <div>not found page</div>;
+};
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
