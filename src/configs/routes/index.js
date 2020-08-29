61bcc88fde0634f01198408deaa0a8eee6d7737a
Balbaal <iqbal.mi20@gmail.com>
2020-08-29 16:49:50 +0800

    add: protected route

--- src/configs/routes/index.js
+++ src/configs/routes/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+
+import { createBrowserHistory } from "history";
 
 // Components
 import Login from "containers/pages/Login";
@@ -10,11 +17,21 @@ const NotFound = () => {
   return <div>not found page</div>;
 };
 
+const authGuard = (Component) => {
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  if (!userData) {
+    return createBrowserHistory().push("/login");
+  } else {
+    return Component;
+  }
+};
+
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={authGuard(Home)} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route component={NotFound} />
