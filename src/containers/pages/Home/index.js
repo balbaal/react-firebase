375659cf85fc6909718a4751b96551109978c566
Balbaal <iqbal.mi20@gmail.com>
2020-08-31 12:10:02 +0800

    add: basic container, layout, margin & button tailwind

--- src/containers/pages/Home/index.js
+++ src/containers/pages/Home/index.js
@@ -84,76 +84,102 @@ class Home extends React.Component {
     const { isLoading, posts } = this.props;
 
     return (
-      <div>
-        <input
-          name="title"
-          value={title}
-          placeholder="title . . ."
-          type="text"
-          onChange={this._handleOnChange}
-        />
-        <br />
-        <textarea
-          onChange={this._handleOnChange}
-          name="description"
-          placeholder="description"
-          value={description}
-        ></textarea>
-        {buttonText === "Update" && (
-          <Button
-            className="mr-2"
-            onClick={this._handleCancelPost}
-            isLoading={isLoading}
-          >
-            Cancel
+      <div className="container mx-auto pt-5">
+        <form>
+          <div className="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="title"
+            >
+              Title
+            </label>
+
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="title"
+              value={title}
+              id="title"
+              placeholder="Title . . ."
+              type="text"
+              onChange={this._handleOnChange}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              for="description"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Description
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={this._handleOnChange}
+              name="description"
+              placeholder="description"
+              value={description}
+              rows="8"
+              id="description"
+            ></textarea>
+          </div>
+          {buttonText === "Update" && (
+            <Button
+              className="mr-2"
+              onClick={this._handleCancelPost}
+              isLoading={isLoading}
+            >
+              Cancel
+            </Button>
+          )}
+          <Button isLoading={isLoading} onClick={this._handleOnSubmit}>
+            {buttonText}
           </Button>
-        )}
-        <Button isLoading={isLoading} onClick={this._handleOnSubmit}>
-          {buttonText}
-        </Button>
+        </form>
 
         <br />
         <br />
 
-        {posts.length !== 0 ? (
-          posts.map((post, i) => {
-            return (
-              <div
-                key={`post-key-${i}`}
-                onClick={() => this._handleOnClickPost(post)}
-                style={{ position: "relative" }}
-              >
-                <h4>title: {post.title}</h4>
-                <h4>description: {post.description}</h4>
-                <h4>created: {post.date}</h4>
-                <Button
-                  style={{ position: "absolute", right: 0, top: 0 }}
-                  isLoading={false}
-                  onClick={(e) => this._handleOnClickDelete(e, post)}
+        <div className="divide-y divide-gray-400">
+          {posts.length !== 0 ? (
+            posts.map((post, i) => {
+              return (
+                <div
+                  className="py-3"
+                  key={`post-key-${i}`}
+                  onClick={() => this._handleOnClickPost(post)}
+                  style={{ position: "relative" }}
                 >
-                  X
-                </Button>
-                <hr />
-              </div>
-            );
-          })
-        ) : isLoading ? (
-          // <h3>loading . . .</h3>
-          <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-            <div className="animate-pulse flex space-x-4">
-              <div className="rounded-full bg-gray-400 h-12 w-12"></div>
-              <div className="flex-1 space-y-4 py-1">
-                <div className="h-4 bg-gray-400 rounded w-3/4"></div>
-                <div className="space-y-2">
-                  <div className="h-4 bg-gray-400 rounded"></div>
-                  <div className="h-4 bg-gray-400 rounded w-5/6"></div>
+                  <h4>Title: {post.title}</h4>
+                  <h4 className="max-w-lg">Description: {post.description}</h4>
+                  <h4>Created: {post.date}</h4>
+                  <Button
+                    style={{ position: "absolute", right: 0, top: 12 }}
+                    isLoading={false}
+                    onClick={(e) => this._handleOnClickDelete(e, post)}
+                  >
+                    X
+                  </Button>
+                </div>
+              );
+            })
+          ) : isLoading ? (
+            // <h3>loading . . .</h3>
+            <div className="border border-gray-300 shadow rounded-md p-4 w-full">
+              <div className="animate-pulse flex space-x-4">
+                <div className="rounded-full bg-gray-400 h-12 w-12"></div>
+                <div className="flex-1 space-y-4 py-1">
+                  <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+                  <div className="space-y-2">
+                    <div className="h-4 bg-gray-400 rounded"></div>
+                    <div className="h-4 bg-gray-400 rounded w-5/6"></div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <h3>there is no post !!</h3>
-        )}
+          ) : (
+            <h3>there is no post !!</h3>
+          )}
+        </div>
       </div>
     );
   }
